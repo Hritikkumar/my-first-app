b384b5a9361b837a1fee54fa77237f146b9a0cc5
Hritik Kumar <kumarhritik38@gmail.com>
2025-05-20 23:01:14 +0530

    Fetch and display movies

---
+++ interfaces/interfaces.d.ts
@@ -0,0 +1,16 @@
+interface Movie {
+    id: number;
+    title: string;
+    adult: boolean;
+    backdrop_path: string;
+    grnre_ids: number[];
+    origunal_language: string;
+    original_title: string;
+    overview: string;
+    popularity;
+    poster_path: string;
+    release_date: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
